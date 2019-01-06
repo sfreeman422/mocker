037ab265178a03a12618aeb6503f414b981a7a6c
Steven Freeman <sfreeman422@gmail.com>
2019-01-06 11:20:55 -0500

    Adjusted get route to post

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/mock', (req, res) => {
+app.post('/mock', (req, res) => {
   console.log(req.body);
   res.send('test');
 });
