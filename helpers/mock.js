3460f266497eab3d2e29ce386d03f043d721280b
Steven Freeman <sfreeman422@gmail.com>
2019-01-06 11:11:45 -0500

    Created server and mocker function

---
+++ helpers/mock.js
@@ -0,0 +1,13 @@
+export function mock(input) {
+  let output = '';
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === ' ') {
+      output += input[i];
+    } else {
+      output += count % 2 === 0 ? input[i].toLowerCase() : input[i].toUpperCase()
+      count++;
+    }
+  }
+  return output;
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mocker = require('./helpers/mock.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/mock', (req, res) => {
+  console.log(req.body);
+  res.send('test');
+});
+
+app.listen(PORT, (e) => {
+  if (e) {
+    console.error(e);
+  } else {
+    console.log("Successfully listening on port 3000");
+  }
+});
