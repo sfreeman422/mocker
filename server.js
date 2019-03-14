05e41aeb1083a93ac6f173fe958bc1236f89bacb
Steven Freeman <sfreeman422@gmail.com>
2019-03-14 14:18:47 -0400

    Styling

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/define", async (req, res) => {
 
   const response = {
     response_type: "in_channel",
-    text: `*${capitalizeFirstLetter(req.body.text)}* \n Definitions:`,
+    text: `*${capitalizeFirstLetter(req.body.text)}*`,
     attachments: formatDefs(defined[0].shortdef)
   };
 
