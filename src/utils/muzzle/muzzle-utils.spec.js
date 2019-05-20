5725cfd0c3193fc1821582822a9625e95db3dc37
Steven Freeman <sfreeman422@gmail.com>
2019-05-19 23:03:44 -0400

    Added further tests to muzzle

--- src/utils/muzzle/muzzle-utils.spec.js
+++ src/utils/muzzle/muzzle-utils.spec.js
@@ -7,17 +7,36 @@ const {
 } = require("./muzzle-utils");
 
 describe("muzzle-utils", () => {
-  it("should add a user to the muzzled array", () => {
-    addUserToMuzzled("test");
-    expect(muzzled.length).to.equal(1);
-    expect(isMuzzled("test")).to.equal(true);
+  const testUser = "test";
+  beforeEach(() => {
+    muzzled.length = 0;
+    addUserToMuzzled(testUser);
+  });
+  describe("addUserToMuzzled()", () => {
+    it("should add a user to the muzzled array", () => {
+      expect(muzzled.length).to.equal(1);
+      expect(isMuzzled(testUser)).to.equal(true);
+    });
   });
 
-  it("should remove a user from the muzzled array", () => {
-    expect(isMuzzled("test")).to.equal(true);
-    expect(muzzled.length).to.equal(1);
-    removeMuzzle("test");
-    expect(isMuzzled("test")).to.equal(false);
-    expect(muzzled.length).to.equal(0);
+  describe("removeMuzzle()", () => {
+    it("should remove a user from the muzzled array", () => {
+      expect(muzzled.length).to.equal(1);
+      expect(isMuzzled(testUser)).to.equal(true);
+      removeMuzzle(testUser);
+      expect(isMuzzled(testUser)).to.equal(false);
+      expect(muzzled.length).to.equal(0);
+    });
+  });
+
+  describe("isMuzzled()", () => {
+    it("should return true if a user is muzzled", () => {
+      expect(isMuzzled(testUser)).to.equal(true);
+    });
+
+    it("should return false if a user is not muzzled", () => {
+      removeMuzzle(testUser);
+      expect(isMuzzled(testUser)).to.equal(false);
+    });
   });
 });
