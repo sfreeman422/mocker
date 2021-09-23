341848c4abe72ecf8c82c19b5f79700914e0a186
steve <sfreeman422@protonmail.com>
2021-09-23 12:48:45 -0400

    Fixed the query

--- src/services/list/list.report.service.ts
+++ src/services/list/list.report.service.ts
@@ -1,15 +1,13 @@
 import Table from 'easy-table';
 import { ReportService } from '../../shared/services/report.service';
-import { List } from '../../shared/db/models/List';
-import { getManager, getRepository } from 'typeorm';
+import { getManager } from 'typeorm';
 import { ListUser } from './ListUser.model';
 
 export class ListReportService extends ReportService {
   // TODO: Add Team ID to the query.
   public async getListReport(): Promise<string> {
     const query = `SELECT u.name, l.text FROM list AS l INNER JOIN slack_user AS u ON u.slackId=l.requestorId;    `;
-    await getManager().query(query);
-    const listReport = await getRepository(List).find();
+    const listReport = await getManager().query(query);
     return this.formatListReport(listReport);
   }
 
