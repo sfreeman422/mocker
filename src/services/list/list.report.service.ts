b92b39d02daf06377e8b75692f256a5a9e3b11a9
steve <sfreeman422@protonmail.com>
2021-09-23 12:46:52 -0400

    Added user

--- src/services/list/list.report.service.ts
+++ src/services/list/list.report.service.ts
@@ -1,18 +1,21 @@
 import Table from 'easy-table';
 import { ReportService } from '../../shared/services/report.service';
 import { List } from '../../shared/db/models/List';
-import { getRepository } from 'typeorm';
+import { getManager, getRepository } from 'typeorm';
+import { ListUser } from './ListUser.model';
 
 export class ListReportService extends ReportService {
   // TODO: Add Team ID to the query.
   public async getListReport(): Promise<string> {
+    const query = `SELECT u.name, l.text FROM list AS l INNER JOIN slack_user AS u ON u.slackId=l.requestorId;    `;
+    await getManager().query(query);
     const listReport = await getRepository(List).find();
     return this.formatListReport(listReport);
   }
 
   private formatListReport(report: any): string {
-    const reportWithoutDate = report.map((listItem: List) => {
-      return { Item: listItem.text };
+    const reportWithoutDate = report.map((listItem: ListUser) => {
+      return { Item: `${listItem.text} - ${listItem.name}` };
     });
 
     return `
